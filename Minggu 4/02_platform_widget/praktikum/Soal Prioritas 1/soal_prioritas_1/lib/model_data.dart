d54950e0983a2bb9dff151056c4e617ad5d303d5
FerdyApriliyanto <ferdya7414@gmail.com>
2023-09-09 13:16:38 +0700

    Temp commit Minggu 4 - 02

---
+++ Minggu 4/02_platform_widget/praktikum/Soal Prioritas 1/soal_prioritas_1/lib/model_data.dart
@@ -0,0 +1,13 @@
+class Contact {
+  final String name, phone;
+
+  Contact({required this.name, required this.phone});
+
+  String getContactName() {
+    return name;
+  }
+
+  String getContactPhone() {
+    return phone;
+  }
+}
